fdc8f40e2ba0c8f701d5c797797202765227299b
Tommie <tommiealcorn@gmail.com>
2019-03-22 12:31:06 -0600

    Commit 1 - Verbal questions

---
+++ js/app.js
@@ -0,0 +1,6 @@
+/*
+    1.  A paramater is a value that is passed into a function,
+        An Argument is what the function receives.
+    2.  Console.log logs to the Console, Return ends the function
+        and gives a value back to where it was called from.
+    3.  The return ends the function, the value cannot be null.
